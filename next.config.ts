4ac65ecebceafdca8640f1c485962529f045b94c
roissi <cyrildegraeve@gmail.com>
2025-08-05 11:47:57 +0200

    generic version 08.25

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Configuration pour les images distantes via Remote Patterns
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "covers.openlibrary.org",
         port: "",
-        // Autorise tous les chemins de couvertures ISBN
         pathname: "/b/isbn/**",
       },
     ],
   },
-  // Vous pouvez ajouter d'autres options de configuration ici
 };
 
 export default nextConfig;
